34ae049b27d416c9dadb2c09d66beb1fac4685f1
Neef Rehman <neef.rehman@gmail.com>
2021-01-18 15:11:35 +0000

    update comment

--- src/components/WebGL/Scene.tsx
+++ src/components/WebGL/Scene.tsx
@@ -18,7 +18,8 @@ import {
     pick,
 } from "../../utils/random";
 
-// createSketch is used to return the sketch function so I can pass in the stateUpdater from the component
+// createSketch is used to return the sketch function so I can pass in setIsLowFrameRate
+// from the component, and update preact state inside the `onFrame` callback.
 const createSketch = (setIsLowFrameRate: StateUpdater<boolean>) => {
     let lowFrameRateAlertHasBeenShown = false;
 
