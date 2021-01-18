39e8c250607cf964832efbf296f8b43311a3f6f6
Neef Rehman <neef.rehman@gmail.com>
2021-01-18 15:33:44 +0000

    update warning copy

--- src/components/UI/LowFrameRateWarning.tsx
+++ src/components/UI/LowFrameRateWarning.tsx
@@ -17,9 +17,10 @@ export const LowFrameRateWarning = ({
         <div className="uiButton modal lowFrameRateModal">
             <p>
                 It looks like the page is {isAlreadyPixelated ? "still" : null}{" "}
-                running slowly. Try refreshing the site in a smaller window, or run
-                a {isAlreadyPixelated ? "more pixelated" : "pixelated"} version of
-                the animation.
+                running slowly. For a smoother animation, try refreshing the site
+                in a smaller window, or run a{" "}
+                {isAlreadyPixelated ? "more pixelated" : "pixelated"} version of
+                the the page.
             </p>
             <div className="buttonContainer">
                 <button className="uiButton">
